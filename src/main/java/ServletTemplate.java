fd347906b32e6f0eaa242f55f7d6499d8a8661d6
Kevin <kevingleijm@gmail.com>
2020-09-29 16:12:39 +0200

    implemented folowing links on webpage via tomcat server

--- src/main/java/ServletTemplate.java
+++ src/main/java/ServletTemplate.java
@@ -1,10 +1,14 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ServletTemplate extends HttpServlet{
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
-
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        RequestDispatcher view = req.getRequestDispatcher("PageTemplate/templateHTMLfile.html");
+        view.forward(req, res);
     }
 }
