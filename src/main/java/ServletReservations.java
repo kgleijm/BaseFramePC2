8215db1825ec0dbe3bbfe1ca79980d1eac850556
Kevin <kevingleijm@gmail.com>
2020-11-02 14:05:35 +0100

    alles samengevoegd

--- src/main/java/ServletReservations.java
+++ src/main/java/ServletReservations.java
@@ -9,37 +9,19 @@ import java.sql.*;
 
 public class ServletReservations extends HttpServlet{
 
-
-
-
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
 
         String email = req.getParameter("email");
-        ResultSet rs = DatabaseManager.getResultsFromQuery("select * from logintable where logintable_emailaddress='"+ email +"' limit 10");
-        req.setAttribute("results", rs);
 
-        try {
-            while (rs.next()) {
-                for (int i = 1; i <= 3; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = rs.getString(i);
-                    System.out.print(columnValue + " ");
-                }
-                System.out.println("");
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(email + " sent reservations request to ReservationServlet");
         }
 
 
 
-        System.out.println(email + " sent reservations request to ReservationServlet");
-
-
-
-
-        RequestDispatcher view = req.getRequestDispatcher("ReservationsTemplate/templateHTMLfile.jsp");
-        view.forward(req, res);
+        //RequestDispatcher view = req.getRequestDispatcher("ReservationsTemplate/templateHTMLfile.jsp");
+        //view.forward(req, res);
     }
 }
